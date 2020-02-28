663e400ae414c2ecdaf7f36c2e3ce24bd690f861
Paulex <paulalexis.verrier@ynov.com>
2020-02-28 17:57:04 +0100

    version2

--- passeplats.sh
+++ passeplats.sh
@@ -8,15 +8,15 @@ LOCK="/tmp/out/lock"
 touch /tmp/out/lock
 
 if [ -d "!$IN" ]; then
-	exit 3
 	echo "The in folder does not exist." 
+	exit 3
 else
 	if [ -d "!$OUT" ]; then
 		mkdir /tmp/out 
 	fi
 	if [ -f "$LOCK" ]; then
-		exit 22
 		echo "The script is already in use"
+		exit 22
 	else
 		gzip -r /tmp/in/ 
 		cp /tmp/in/* /tmp/out/
